ab0082143ba8541ee3436d3e2769bb7eb1266c0c
Beth Jackson <Mbeth.ashton@gmail.com>
2020-06-10 14:30:56 -0400

    OTP-Servers-2

--- lib/simplestack/server.ex
+++ lib/simplestack/server.ex
@@ -8,4 +8,8 @@ defmodule Simplestack.Server do
   def handle_call(:pop, _from, [ first | rest ]) do
     {:reply, first, rest}
   end
+  
+  def handle_cast({:push, value}, current_list) do
+    {:noreply, [value | current_list]}
+  end
 end
